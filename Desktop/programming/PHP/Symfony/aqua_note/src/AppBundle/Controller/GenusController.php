ca156d1051bee9c9f1ed966b293cd086e56a98b8
Dhrsaad13 <dhrsaad13@gmail.com>
2017-09-17 09:28:04 +0100

    sa

---
+++ Desktop/programming/PHP/Symfony/aqua_note/src/AppBundle/Controller/GenusController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace  AppBundle\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+class GenusController
+{
+   /**
+     * @Route("/genus/{genusName}")
+     */
+
+    public function showAction($genusName)
+    {
+        return new Response('The genus: '.$genusName);
+    }
+}
+
+
+
+
+ ?>
